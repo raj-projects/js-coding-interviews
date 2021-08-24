8c485208ba18bdd2d1a2fa95824ea567e6ff894d
raj-projects <niit.rahulraj@gmail.com>
2021-08-24 16:39:33 +0530

    1 file modified, 1 added

---
+++ js/array/second-largest-no.js
@@ -0,0 +1,21 @@
+const arrEle = [5, 22, 3, 43, 72, 8, 88, 90, 4, 6, 33];
+
+const getSecondLargestNum = (arr = []) => {
+  largestNum = arr[0];
+  secondLargestNum = arr[0];
+
+  // Second Largest
+  for (i = 0; i < arr.length; i++) {
+    if (arr[i] > largestNum) {
+      secondLargestNum = largestNum;
+      largestNum = arr[i];
+    } else if (arr[i] > secondLargestNum) {
+      secondLargestNum = arr[i];
+    }
+  }
+
+  console.log(secondLargestNum)
+  return secondLargestNum;
+};
+
+getSecondLargestNum(arrEle);
