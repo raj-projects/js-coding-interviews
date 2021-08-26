7446b8e2e83196e42806a27a3babe632067cff7a
it-dreams <rahulraj.holi888@gmail.com>
2021-08-26 19:34:02 +0530

    fibonacci series

---
+++ js/math/fabonacci-series.js
@@ -0,0 +1,18 @@
+/*
+    17) Find the nth Number in fibonacci series using Iterative and Recursive Approach.
+
+    Formula => f(n) = f(n-2) + f(n-1) 
+*/
+
+const nthFib = (n) => {
+    const fibArray = [0, 1];
+
+    for (let i = 2; i < n; i++) {
+
+        // f(n) = f(n-2) + f(n-1) // Formula
+        fibArray.push(fibArray[i - 2] + fibArray[i - 1]);
+    }
+    return fibArray;
+}
+
+console.log(nthFib(50));
