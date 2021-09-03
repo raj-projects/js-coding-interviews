b374ffa195d2a9c5c662f0d239290d8de4b19260
it-dreams <rahulraj.holi888@gmail.com>
2021-09-03 11:18:40 +0530

    updated question

--- js/string/calculate-distance-word.js
+++ js/string/calculate-distance-word.js
@@ -1,28 +1,33 @@
+/*
+    Minimum distance between 2 words counting the number of characters from middle of both words. E.g. ABC is XYZ and ABC & XYZ are two distinct words. 
+*/
+
+
+
 let word = "abc is abc and then xyz";
+let w1 = 'abc';
+let w2 = 'xyz';
 
-w1 = 'abc';
-w2 = 'xyz';
-
-function calculateDistance(w1, w2) {
-  w1Index = findIndexOfWord(w1);
-  w2Index = findIndexOfWord(w2);
-  
-  console.log(w1Index)
-  
-  let distance = w1Index > w2Index ? w1Index-w1Index-1 : w2Index-w1Index-1;
-  console.log(distance)
-}
-
-function findIndexOfWord(data) {
+function findIndexOfWord(str) {
   let matchedIndex = 0;
-  console.log(data);
+  console.log(str);
   
   for(let i=0; i<word.split(" ").length; i++) {
-    if(data === word.trim().split(" ")[i]) {
+    if(str === word.trim().split(" ")[i]) {
       matchedIndex = i;
     }
   }
   return matchedIndex;
 }
+
+function calculateDistance(w1, w2) {
+    w1Index = findIndexOfWord(w1);
+    w2Index = findIndexOfWord(w2);
+    
+    console.log(w1Index)
+    
+    let distance = w1Index > w2Index ? w1Index-w1Index-1 : w2Index-w1Index-1;
+    console.log(distance)
+  }
 
 calculateDistance(w1, w2)
